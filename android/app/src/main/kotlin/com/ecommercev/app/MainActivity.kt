b4a69e184fff0d54d3220739c175049ecb72082f
Kolade Modupe <Kolademodupe007@gmail.com>
2023-12-25 15:18:00 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/ecommercev/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ecommercev.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:ecommerce_v1/core/app_export.dart';
+
+class ApiClient {}
